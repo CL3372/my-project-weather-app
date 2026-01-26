a4a7ee05ee64940131d17b7206fa5be790a6c8b2
CL3372 <chocolatesdomjose@gmail.com>
2026-01-26 18:14:23 +0000

    inserted apikey and apiUrl and also inserted axios link

--- script.js
+++ script.js
@@ -11,3 +11,8 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.ariaValueMax;
 }
+
+let apiKey = "aa9340df384978aa7t1a53a8fabcc2o1";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
