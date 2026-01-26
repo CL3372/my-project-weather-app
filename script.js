d7a397230cee04decd9db5068baaf2d84e2e4335
CL3372 <chocolatesdomjose@gmail.com>
2026-01-26 18:23:28 +0000

    inserted string of days Sunday to Saturday

--- script.js
+++ script.js
@@ -16,3 +16,30 @@ let apiKey = "aa9340df384978aa7t1a53a8fabcc2o1";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
