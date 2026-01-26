91ca2b275596cd74b829aac557249b4a20bc0ff0
CL3372 <chocolatesdomjose@gmail.com>
2026-01-26 17:52:02 +0000

    input Carla Louro name on footer
    inserted java script functions for weather

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert("Hello Carla");
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
